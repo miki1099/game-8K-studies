3802c5c7a541e44fe671fe9047f0d6d6238e905f
Michal G <miki.glodek@wp.pl>
2020-11-25 18:41:26 +0100

    Added lib description to readme and annotation to Item clas

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # game-8K-studies
 Strategic himalaism game on studies.
 Game take pattern from board games.
+
+#Libraries:
+-lombok
--- src/gameLogic/Item.java
+++ src/gameLogic/Item.java
@@ -1,5 +1,8 @@
 package gameLogic;
 
+import lombok.Data;
+
+@Data
 public abstract class Item {
     protected byte nightImpactMod;
     protected byte moveImpactMod;
@@ -11,23 +14,6 @@ public abstract class Item {
     @Override
     public String toString() {
         return name;
-    }
-
-
-    public byte getNightImpactMod() {
-        return nightImpactMod;
-    }
-
-    public byte getMoveImpactMod() {
-        return moveImpactMod;
-    }
-
-    public void setNightImpactMod(byte nightImpactMod) {
-        this.nightImpactMod = nightImpactMod;
-    }
-
-    public void setMoveImpactMod(byte moveImpactMod) {
-        this.moveImpactMod = moveImpactMod;
     }
 
     public String getName() {
